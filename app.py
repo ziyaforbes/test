5eabd990d5979262369cb80290859db18d077724
ziyaforbes <160036947+ziyaforbes@users.noreply.github.com>
2024-04-08 18:06:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 st.write('# NY Housing Dataset')
-st.write('## Run Streamlit on Colab')
-st.write('## Run Streamlit on Colab')
-st.write('## Run Streamlit on Colab')
-st.write('## Run Streamlit on Colab')
-st.write('## Run Streamlit on Colab')
-st.write('## Run Streamlit on Colab')
 
